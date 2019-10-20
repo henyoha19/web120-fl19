2d14dd78fee0f9087d1ddbee5824c869d49b5f77
Henock W <46727864+henyoha19@users.noreply.github.com>
2019-10-20 14:40:38 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php include "includes/header.php";?>
+
+<!-- MAKE SURE YOU GET YOUR (3) IMAGES SAVED INTO YOUR IMAGES FOLDER -->
+ <img src="images/desktop.jpg" class="desktop" alt="" />
+ <img src="images/phone.jpg" class="phone" alt="" />
+ <p>My Name is <b> Henock</b>. This is my portal website. I am a student at the Seattle Central College Information Technology Department. I am interested to learn more about web and database development. My two years plan is to complete the database development certificate program here in SCC. ;)</p>
+<p>I born and grown up in east Africa. I moved to Seattle with my family. I have two daughters;)</p>
+<p>As shown in the picture, i like natural areas such as forest, waterfall, grassland and mountains areas! ;)</p>
+</section>
+<!-- END LEFT COL -->
+
+<!-- START RIGHT COL -->
+<aside>
+ <h3>Right Column</h3>
+ <img src="images/tablet.jpg" class="tablet" alt="" />
+ <p>Lorem Ipsum place holder text goes here for now</p>
+ <p>Lorem Ipsum place holder text goes here for now</p>
+</aside>
+<!-- END RIGHT COL -->
+ 
+<?php include "includes/footer.php";?>
+
+
---
+++ table.php
@@ -0,0 +1,64 @@
+<?php include "includes/header.php";?>
+
+<style>
+    table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 2px solid;
+  text-align: left;
+  padding: 8px;
+}
+</style>
+ <table>
+     <caption> My Menu</caption>  
+        <tr>
+          <th> Food Name</th>
+          <th>Description</th>
+          <th>Price</th>
+        </tr>
+
+        <tr>
+            <td>Spaghetti</td>
+            <td>Pasta, grounded beef,onion,paper,tomato and garlic </td>
+            <td>$25</td>
+        </tr>
+       <tr>
+            <td>Tuna Salad</td>
+            <td>lattece,tomato,potato,avocado,onion,cucumber and tuna</td>
+            <td>$28</td>
+      </tr>
+        <tr>
+            <td>Rice with beef</td>
+            <td>Rice,beef,onion,garlic and papper</td>
+            <td>$23</td>
+      </tr>
+        <tr>
+            <td>Fried Fish</td>
+            <td>Fish,onion,garlic and papper</td>
+            <td>$24</td>
+      </tr>
+          </table>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+<?php include "includes/footer.php";?>
