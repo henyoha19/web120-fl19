b4f85a057f380ced9f19c35614e16d489f23b443
Henock W <46727864+henyoha19@users.noreply.github.com>
2019-10-27 17:41:41 -0700

    Add files via upload

---
+++ web120/contact.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Henock's WEB120 Portal - Edit this code to make it your own!</title>
+    <meta name="viewport" content="width=device-width" />
+    <meta name="robots" content="noindex,nofollow" />
+    <meta charset="utf-8">
+    <script src="https://use.fontawesome.com/6a71565c22.js"></script>
+    <link rel="stylesheet" href="css/nav.css" />
+    <link rel="stylesheet" href="css/portal.css" />
+    <link rel="stylesheet" href="css/form.css" />
+    <link rel="stylesheet" href="css/table.css" />
+</head>
+
+<body>
+<!-- START WRAPPER -->
+<main class="wrapper">
+<header>
+  <h1><a href="index.php"><i class="logo fa fa-home"></i> Henock's WEB120 Portal</a></h1>
+  <nav>
+    <ul class="topnav" id="myTopnav">
+      <li><a href="index.php" class="selected">Welcome</a></li>
+      <li><a href="big/index.php">Big</a></li>
+      <li><a href="aia.php">AIA</a></li>
+      <li><a href="flowchart.php">Flowchart/Layout</a></li>
+      <li><a href="fp/index.php">Final Project</a></li>
+      <li><a href="contact.php">Contact Henock</a></li>
+      <li class="icon"> <a href="javascript:void(0);" onclick="myFunction()">&#9776;</a> </li>
+    </ul>
+  </nav>
+</header>
+        
+<!-- START LEFT COL -->
+<section>
+ <h2 class="pageID">Contact Henock</h2>
+
+<?php
+        /*
+         * Below are 2 different forms to be re-used       
+         * 
+         * Only use one at a time, comment out the other!       
+         *
+         */
+
+        include 'includes/contact_include.php'; #site keys & code here
+    
+        $toAddress = "henockyohaK@gmail.com";  //place your/your client's email address here
+        $toName = "Henock"; //place your client's name here
+        $website = "Web120 Contact File Test";  //place NAME of your client's website
+
+        echo loadContact('simple.php');#demonstrates a simple contact form
+        //echo loadContact('multiple.php');#demonstrates multiple form elements
+
+	?>
+<footer>
+  <p><small>&copy; <?=date('Y')?> by <a href="contact.php" target="_blank">Henock!</a>, All Rights Reserved ~ <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small></p>
+</footer>
+<!-- END Footer --> 
+</main>
+<!-- END WRAPPER -->
+
+    <!-- JavaScript associated with the W3Schools.com Top Navigation Response Exercise --> 
+    <script>
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+     </script>
+</body>
+</html>
+
---
+++ web120/table.php
@@ -0,0 +1,42 @@
+<?php include "includes/header.php";?>
+
+  <table >
+        <caption>
+          My Menu
+        </caption>
+        <thead>
+        <tr>
+          <th scope="col">Food Name</th>
+          <th scope="col">Description</th>
+          <th scope="col">Price</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+             <td>Spaghetti</td>
+             <td>Pasta, grounded beef,onion,tomato </td>
+             <td>$25</td>
+        </tr>
+        <tr>
+            <td>Tuna Salad</td>
+            <td>lattece,onion and tuna</td>
+            <td>$28</td>
+        </tr>
+        <tr>
+            <td>Rice with beef</td>
+            <td>Rice,beef,onion,garlic and papper</td>
+            <td>$23</td>
+        </tr>
+        <tr>
+            <td>Fried Fish</td>
+            <td>Fish,onion,garlic and papper</td>
+            <td>$24</td>
+        </tr>
+        </tbody>
+      </table>
+
+
+
+
+ 
+<?php include "includes/footer.php";?>
